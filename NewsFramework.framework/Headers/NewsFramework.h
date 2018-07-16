ea8683d5dc6f9b3749b34c2cf84dc43bc8fd1556
epidhinios <epidhinios@gmail.com>
2018-07-16 16:50:03 -0500

    Initial `commit

---
+++ NewsFramework.framework/Headers/NewsFramework.h
@@ -0,0 +1,19 @@
+//
+//  NewsFramework.h
+//  NewsFramework
+//
+//  Created by TCS_MX on 7/16/18.
+//  Copyright © 2018 TCS_MX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NewsFramework.
+FOUNDATION_EXPORT double NewsFrameworkVersionNumber;
+
+//! Project version string for NewsFramework.
+FOUNDATION_EXPORT const unsigned char NewsFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NewsFramework/PublicHeader.h>
+
+
